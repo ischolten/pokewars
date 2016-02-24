80f81b3938a6e73a455b5831c678ba3ddfadbb2d
Iris <ischolten@hotmail.com>
2016-02-24 15:25:26 -0500

    commit before push: added scenes and buttons

---
+++ assets/Scripts/SceneChangeRegisterScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneChangeRegisterScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void RegisterButtonClick()
+    { 
+        UnityEngine.SceneManagement.SceneManager.LoadScene("Home");
+    }
+}
