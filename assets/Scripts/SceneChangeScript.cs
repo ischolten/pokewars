b968e953f8b02cc97f0d86e05fb67d5c9ff4dbaa
Iris <ischolten@hotmail.com>
2016-03-15 22:27:50 -0400

    buttons load scenes now

--- assets/Scripts/SceneChangeScript.cs
+++ assets/Scripts/SceneChangeScript.cs
@@ -5,19 +5,32 @@ public class SceneChangeScript : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-	
+        
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+
 	}
 
-    public void ButtonClick(string scenename)
+    public void onGoToProfile()
     {
-        if (!(scenename.Equals(null)))
-        {
-            UnityEngine.SceneManagement.SceneManager.LoadScene(scenename);
-        }
+        UnityEngine.SceneManagement.SceneManager.LoadScene("Profile");
+    }
+
+    public void onGoToRegister()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("Register");
+    }
+
+    public void onGoToWelcome()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("Welcome");
+    }
+
+    public void onMenu()
+    {
+        print("MENU");
+        
     }
 }
