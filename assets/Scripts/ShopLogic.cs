af20481d5506fbf5dc9b3f426c0b7cf9c11fc747
jnugent6 <jnugent6@gatech.edu>
2016-04-28 16:27:59 -0400

    ETRRTWET

---
+++ assets/Scripts/ShopLogic.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShopLogic : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void onClickBack()
+	{
+		UnityEngine.SceneManagement.SceneManager.LoadScene("Profile");
+	}
+
+	public void onClickGloves()
+	{
+		UnityEngine.SceneManagement.SceneManager.LoadScene("Store");
+	}
+
+	public void onClickAccessories()
+	{
+		UnityEngine.SceneManagement.SceneManager.LoadScene("StoreTab2");
+	}
+
+//	public void purchaseGlove1()
+//	{
+//		int gold = 300;
+//	}
+//
+//	public void purchaseGlove2()
+//	{
+//		int gold = 500;
+//	}
+//
+//	public void purchaseGlove3()
+//	{
+//		int gold = 750;
+//	}
+//
+//	public void purchaseGlove4()
+//	{
+//		int gold = 999;
+//	}
+//
+//	public void purchaseCharm1()
+//	{
+//		int gold = 300;
+//	}
+//
+//	public void purchaseCharm2()
+//	{
+//		int gold = 500;
+//	}
+//
+//	public void purchasePowerup1()
+//	{
+//		int gold = 750;
+//	}
+//
+//	public void purchasePotion1()
+//	{
+//		int gold = 999;
+//	}
+}
