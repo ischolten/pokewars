a09ad46429b6c0145fb90bdb49b50ba087854392
Joan Chen <jkchen720@gmail.com>
2016-04-29 00:12:09 -0400

    update xghcrdyhstet

---
+++ assets/Scripts/EnemyModel.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyModel
+{
+	static public int experience;
+	static public int health;
+	static public int strength;
+	static public int speed;
+	static public int gold;
+	static public int rank;
+	static public ArrayList items;
+	static public string name;
+	static public int id;
+
+	static public int curr_zone;
+	static public int curr_field;
+}
